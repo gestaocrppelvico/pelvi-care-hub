813a8f9167861fb28243e8cb68c61389ae9783d7
gestaocrppelvico <gestao.crppelvico@gmail.com>
2026-07-30 16:46:14 -0300

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router-dom";
-import { Sidebar } from "./Sidebar"; 
 
 export function Layout() {
   return (
-    <div className="flex h-screen">
-      <Sidebar />
-      <main className="flex-1 overflow-auto p-4">
-        <Outlet />
-      </main>
+    <div className="min-h-screen bg-gray-50">
+      <div className="flex">
+        <main className="flex-1 p-4">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
