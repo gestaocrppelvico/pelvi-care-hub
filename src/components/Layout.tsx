af407e4f6691987a97894fab8bb0764664b45036
gestaocrppelvico <gestao.crppelvico@gmail.com>
2026-07-30 16:04:16 -0300

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import { Sidebar } from "./Sidebar"; // verifique se o Sidebar existe
+import { Sidebar } from "./Sidebar"; 
 
 export function Layout() {
   return (
