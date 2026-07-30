c31f4aac908c2d8ccee7367a66710e89138b298c
gestaocrppelvico <gestao.crppelvico@gmail.com>
2026-07-30 18:34:17 -0300

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
+import { BottomNav } from "./BottomNav";
+import { AppHeader } from "./AppHeader";
 
 export function Layout() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <div className="flex">
-        <main className="flex-1 p-4">
-          <Outlet />
-        </main>
-      </div>
+    <div className="min-h-screen bg-gray-50 flex flex-col">
+      <AppHeader />
+      <main className="flex-1 p-4 pb-20">
+        <Outlet />
+      </main>
+      <BottomNav />
     </div>
   );
 }
